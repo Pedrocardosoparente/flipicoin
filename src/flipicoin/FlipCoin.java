0b2c30a39b3a2fbb53d7455ead2962e82319863e
Aluno <Aluno@10.85.198.36>
2024-06-05 16:39:25 -0300

    filipi

---
+++ src/flipicoin/FlipCoin.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class flipCoin {
+
+    public static void main(String[] args) {
+
+        // 50% chance heads, 50% chance tails
+        Random rand = new Random();
+        double chance = rand.nextDouble();
+        System.out.println(chance);
+        }
+    }
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Aluno
+ */
+ 
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+
